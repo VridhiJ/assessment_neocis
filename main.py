0626ff4ad6c85d099abbc17e45a8bac50b18ddec
VridhiJ <vridhi@seas.upenn.edu>
2025-04-20 19:56:22 -0400

    part 1

---
+++ main.py
@@ -0,0 +1,8 @@
+from object_3d import Object3D
+from viewer import WireframeViewer
+
+if __name__ == "__main__":
+    object3d = Object3D("object.txt")
+    viewer = WireframeViewer(object3d)
+    viewer.run()
+
