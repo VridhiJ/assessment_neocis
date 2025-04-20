0b275f22416539e88aac9910b38deac80c7cff04
VridhiJ <vridhi@seas.upenn.edu>
2025-04-20 19:55:34 -0400

    comma-separated

--- object_loader.py
+++ object_loader.py
@@ -8,20 +8,20 @@ def load_object(filepath):
     with open(filepath) as file:
         lines = file.readlines()
 
-    line1 = lines[0].strip().split()
+    line1 = lines[0].strip().split(",")
     num_vertices = int(line1[0])
     num_faces = int(line1[1])
 
     
 
     for i in range(1, num_vertices+1):
-        parts = lines[i].strip().split()
+        parts = lines[i].strip().split(",")
         vertex_id = int(parts[0])
         x, y, z = map(float, parts[1:])
         vertices[vertex_id] = Vertex(vertex_id, x, y, z)     
 
     for j in range(num_vertices + 1, num_faces + num_vertices + 1):
-        v1, v2, v3 = map(int,lines[i].strip().split())
+        v1, v2, v3 = [int(float(x)) for x in lines[j].strip().split(",")]
         faces.append(Face(v1, v2, v3))
 
     return vertices, faces    
